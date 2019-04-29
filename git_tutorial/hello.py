1066cd8f6581aef177c98f7c009e365953183c80
dykim4040 <50095524+dykim4040@users.noreply.github.com>
2019-04-29 16:18:11 +0900

    Update hello.py

--- git_tutorial/hello.py
+++ git_tutorial/hello.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
+
 //for command line git tutorial
 //local repository modification
 print("hello world")
-=======
+
 print("hello world")
-
-//for command line git tutorial
-//github modification
-
 //hello pussy 
 >>>>>>> origin/master
